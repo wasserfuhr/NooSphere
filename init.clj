20b447ebf426106789d189d618a8c112c61897ad
RaWa <rainerwasserfuhr+GitHub@gmail.com>
2016-02-21 13:14:52 +0100

    ~init.clj

--- init.clj
+++ init.clj
@@ -1,3 +1,8 @@
 (fn[rq rs]
- (hiccup.core/html"<!DOCTYPE html>"[:html[:head
-  [:title"BitCrush"]](slurp"https://dresdenlabs.appspot.com/at")[:textarea]]))
+(if(=(.getParameter rq"a")"save")
+ "saved"
+(hiccup.core/html"<!DOCTYPE html>"[:html[:head
+  [:title"BitCrush"]][:body(slurp"https://dresdenlabs.appspot.com/at")
+[:form
+[:textarea {:name"t"}]
+[:input{:type"submit":name"a":value"save"}]]]]))))
