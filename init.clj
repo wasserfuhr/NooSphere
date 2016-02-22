cf4f27061f4098802fb67b4b8eff605739b89f32
RaWa <rainerwasserfuhr+GitHub@gmail.com>
2016-02-22 15:56:17 +0100

    ~init.clj

--- init.clj
+++ init.clj
@@ -1,5 +1,6 @@
 (fn[rq rs](let [
-   t (.getParameter rq"t")
+   t(.getParameter rq"t")
+   e(.getParameter rq"e")
    h
     (fn [m] ; https://gist.github.com/kisom/1698245
      (let [h (java.security.MessageDigest/getInstance "SHA-256")]
@@ -7,15 +8,15 @@
       (.digest h)))
    ;http://stackoverflow.com/questions/10062967/clojures-equivalent-to-pythons-encodehex-and-decodehex
    hh (fn [m] (apply str (map #(format "%02x" %) m)))]
-(if(=(.getParameter rq"a")"e")
-  "eval!"
+(if(.getParameter rq"e")
+  (eval (read-string(slurp e)))
  (if(=(.getParameter rq"a")"save")(do
   (spit(str "1220"(hh(h(.getBytes t)))".txt") t)
   (str"saved"(hh(h(.getBytes t)))))
 (hiccup.core/html"<!DOCTYPE html>"[:html[:head
   [:title"BitCrush"]][:body(slurp"https://dresdenlabs.appspot.com/at")
 (map (fn [f](if(.startsWith (.getName f)"1220")
- [:a{:href "(.getName f)"}(.getName f)]))(.listFiles(java.io.File.".")))
+ [:a{:href(str"?e="(.getName f))}(.getName f)]))(.listFiles(java.io.File.".")))
 [:form
  [:textarea {:name"t"}]
  [:input{:type"submit":name"a":value"save"}]]]]))))))
