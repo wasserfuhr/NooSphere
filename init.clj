4d578157e611d44487e9dfa6cb490a39e0465707
RaWa <rainerwasserfuhr+GitHub@gmail.com>
2016-02-22 17:03:09 +0100

    ~init.clj

--- init.clj
+++ init.clj
@@ -1,22 +1,18 @@
-(fn[rq rs](let [
-   t(.getParameter rq"t")
-   e(.getParameter rq"e")
-   h
-    (fn [m] ; https://gist.github.com/kisom/1698245
-     (let [h (java.security.MessageDigest/getInstance "SHA-256")]
-      (. h update m)
-      (.digest h)))
+(fn[rq rs](let[
+  t(.getParameter rq"t")
+  e(.getParameter rq"e")
+  h(fn[m](let[; https://gist.github.com/kisom/1698245
+   h(java.security.MessageDigest/getInstance"SHA-256")]
+   (. h update m)(.digest h)))
    ;http://stackoverflow.com/questions/10062967/clojures-equivalent-to-pythons-encodehex-and-decodehex
-   hh (fn [m] (apply str (map #(format "%02x" %) m)))]
-(if(.getParameter rq"e")
-  (eval (read-string(slurp e)))
+  hh(fn[m](apply str(map #(format"%02x" %)m)))]
+(if(.getParameter rq"e")((eval (read-string(slurp e)))rq rs)
  (if(=(.getParameter rq"a")"save")(do
   (spit(str "1220"(hh(h(.getBytes t)))".txt") t)
   (str"saved"(hh(h(.getBytes t)))))
-(hiccup.core/html"<!DOCTYPE html>"[:html[:head
-  [:title"BitCrush"]][:body(slurp"https://dresdenlabs.appspot.com/at")
-(map (fn [f](if(.startsWith (.getName f)"1220")
- [:a{:href(str"?e="(.getName f))}(.getName f)]))(.listFiles(java.io.File.".")))
-[:form
- [:textarea {:name"t"}]
- [:input{:type"submit":name"a":value"save"}]]]]))))))
+(hiccup.core/html"<!DOCTYPE html>"[:html[:head[:title"/edit « ?"]][:body(slurp"https://dresdenlabs.appspot.com/at")
+ (map(fn[f](if(.startsWith(.getName f)"1220")
+  [:a{:href(str"?e="(.getName f))}(.getName f)]))(.listFiles(java.io.File.".")))
+ [:form
+  [:textarea {:name"t"}]
+  [:input{:type"submit":name"a":value"save"}]]]]))))))
